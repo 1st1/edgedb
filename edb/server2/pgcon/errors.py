1d58f69ef6e6de3ceb040138a4916e3dbad7ec84
Yury Selivanov <yury@magic.io>
2018-12-19 17:38:21 -0500

    wp

---
+++ edb/server2/pgcon/errors.py
@@ -0,0 +1,25 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2008-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+class BackendError(Exception):
+
+    def __init__(self, *, fields):
+        msg = fields.get('M', f'error code {fields["C"]}')
+        self.fields = fields
+        super().__init__(msg)
